223ad6ca1ea71a403eb46ed42efd0297687241c0
tianci <superealboom@163.com>
2025-03-03 10:48:35 +0800

    示例项目初始化，并增加第一个示例，Java 基础中判断字符串是否中文

---
+++ README.md
@@ -0,0 +1,4 @@
+## 项目所有示例
+### java-basic
+> java 基础示例项目
+1. 检测字符串中是否全部为中文。`cn.afuo.character.ChineseCharacter`
---
+++ java-basic/src/main/java/cn/afuo/character/ChineseCharacter.java
@@ -0,0 +1,41 @@
+package cn.afuo.character;
+
+/**
+ * 汉字
+ */
+public class ChineseCharacter {
+
+
+    public static void main(String[] args) {
+        String str = "我㷋";
+        System.out.println(isChineseByRegular(str));
+        System.out.println(isChineseByExpand(str));
+    }
+
+
+    /**
+     * 通过正则表达式判断是否是汉字
+     * 基本汉字的Unicode - CJK统一表意文字 - 无法匹配扩展汉字
+     */
+    private static boolean isChineseByRegular(String strName) {
+        if (strName.matches("^[\\u4e00-\\u9fa5]+$")) {
+            return true;
+        }
+        return false;
+    }
+
+
+    /**
+     * 通过Unicode判断是否是汉字
+     * 匹配所有汉字字符，包括基本汉字和扩展汉字
+     */
+    private static boolean isChineseByExpand(String strName) {
+        if (strName.matches("^[\\p{Script=Han}]+$")) {
+            return true;
+        }
+        return false;
+    }
+
+
+
+}
